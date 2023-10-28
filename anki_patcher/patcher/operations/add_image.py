9f6255724f1239339a6e4328370423f15b6479b8
Alexander <alexanderweichart@fastmail.com>
2023-10-28 18:03:26 +0200

    impl: better retry for image downlaod

--- anki_patcher/patcher/operations/add_image.py
+++ anki_patcher/patcher/operations/add_image.py
@@ -1,16 +1,21 @@
 import requests
 import os
+from PIL import Image
+import io
+import imghdr
 
 from anki_patcher.patcher.anki import clean_string_of_html_tags
 from anki_patcher.util import parse_config, parse_env, parse_fields
 
-def execute (card_id, fields, config):
+MAX_IMAGE_RETRIES = 5
+
+def execute(card_id, fields, config):
     # load env vars via helper function
     env = parse_env(["ANKI_MEDIA_FOLDER", "GOOGLE_API_KEY", "CX"])
     # load config vars via helper function
     [search_input_field_name, image_field_name] = parse_config(["search_input_field_name", "image_field_name"], config)
     # load card field values via helper function
-    [search_input, existing_image] = parse_fields([search_input_field_name, image_field_name,], fields)
+    [search_input, existing_image] = parse_fields([search_input_field_name, image_field_name], fields)
 
     # clean search input of html tags
     clean_query = clean_string_of_html_tags(search_input)
@@ -18,20 +23,36 @@ def execute (card_id, fields, config):
     add_image_to_card(card_id, clean_query, existing_image, image_field_name, env)
 
 
+def is_valid_image(filepath):
+    try:
+        img = Image.open(filepath)
+        img.verify()
+        return True
+    except:
+        return False
+
+
+def get_image_type(image_data):
+    try:
+        image = Image.open(io.BytesIO(image_data))
+        return image.format.lower()
+    except Exception as e:
+        return None
+
 def add_image_to_card(card_id, query, existing_image, image_field_name, env):
     if existing_image != "":
         print(f"Skipping card {card_id} as it already has an image")
         return
 
     [media_folder, google_api_key, cx] = env
-
     print(f"Searching for image: {query}")
+
     params = {
         "q": query,
         "searchType": "image",
         "key": google_api_key,
         "cx": cx,
-        "num": 1,  # Number of images to return. Set to 1 to get the first image.
+        "num": 5,  # Number of images to return. Set to 1 to get the first image.
         "fileType": "jpg"
     }
     try:
@@ -39,34 +60,44 @@ def add_image_to_card(card_id, query, existing_image, image_field_name, env):
         if response.status_code != 200:
             raise Exception(f"Received non-200 response from Google Custom Search API: {response.status_code}")
         data = response.json()
+
         if "items" not in data or not data["items"]:
             print(f"No images found for query: {query}")
             return
         
-        image_url = data["items"][0]["link"]
+        for attempt in range(MAX_IMAGE_RETRIES):
+            image_url = data["items"][attempt]["link"]
+            image_data = requests.get(image_url).content
+            image_type = get_image_type(image_data)
 
-        # Download the image
-        image_data = requests.get(image_url).content
-        image_filename = f"{card_id}.jpg"
-        
-        # Save the image to the Anki media folder
-        filepath = os.path.join(media_folder, image_filename)
+            valid_image_types = ["jpeg", "png", "jpg"]
+            if image_type not in valid_image_types:
+                print(f"Invalid image downloaded, retrying... ({attempt + 1}/3)")
+                continue
 
-        # override existing file:
-        with open(filepath, 'wb') as f:
-            f.write(image_data)
+            image_filename = f"{card_id}.{image_type}"
+            filepath = os.path.join(media_folder, image_filename)
 
-        # Update the Anki card to reference the image by its filename
-        field_data = f'<img src="{image_filename}">'
-        params = {
-            "note": {
-                "id": card_id,
-                "fields": {image_field_name: field_data}
-            }
-        }
-        from anki_patcher.patcher.anki import invoke
-        invoke("updateNoteFields", params)
-        print(f"Added image to card {card_id}")
+            with open(filepath, 'wb') as f:
+                f.write(image_data)
+            
+            if is_valid_image(filepath):
+                # Update the Anki card to reference the image by its filename
+                field_data = f'<img src="{image_filename}">'
+                params = {
+                    "note": {
+                        "id": card_id,
+                        "fields": {image_field_name: field_data}
+                    }
+                }
+                from anki_patcher.patcher.anki import invoke
+                invoke("updateNoteFields", params)
+                print(f"Added image to card {card_id}")
+                break
+            else:
+                print(f"Invalid image downloaded, retrying... ({attempt + 1}/3)")
+                if attempt == MAX_IMAGE_RETRIES - 1:
+                    print("Failed to download a valid image after 3 attempts")
     except Exception as e:
         print(f"Error downloading image: {e}")
         return
