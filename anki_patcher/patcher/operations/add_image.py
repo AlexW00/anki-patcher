443da9da13dd9ec0ee75543b46142ac38939b198
Alexander <alexanderweichart@fastmail.com>
2023-11-04 18:43:03 +0100

    fix img script

--- anki_patcher/patcher/operations/add_image.py
+++ anki_patcher/patcher/operations/add_image.py
@@ -5,21 +5,34 @@ import io
 import timeout_decorator
 
 
-from anki_patcher.util import parse_config, parse_env, parse_fields, remove_html_tags_bs
+from anki_patcher.util import parse_config, parse_env, parse_fields, remove_html_tags
 
 MAX_IMAGE_RETRIES = 5
+
 
 @timeout_decorator.timeout(10)
 def execute(card_id, fields, config):
     # load env vars via helper function
     env = parse_env(["ANKI_MEDIA_FOLDER", "GOOGLE_API_KEY", "CX"])
     # load config vars via helper function
-    [search_input_field_name, image_field_name] = parse_config(["search_input_field_name", "image_field_name"], config)
+    [search_input_field_name, image_field_name] = parse_config(
+        ["search_input_field_name", "image_field_name"], config
+    )
     # load card field values via helper function
-    [search_input, existing_image] = parse_fields([search_input_field_name, image_field_name], fields)
+    [search_input, existing_image] = parse_fields(
+        [search_input_field_name, image_field_name], fields
+    )
 
     # clean search input of html tags
-    clean_query = remove_html_tags_bs(search_input)
+    clean_query = remove_html_tags(search_input)
+    # split by " "
+    clean_query = clean_query.split(", ")
+    # remove empty strings
+    clean_query = list(filter(None, clean_query))
+    # take only the first 5 words
+    clean_query = clean_query[:2]
+    # join back with " "
+    clean_query = ", ".join(clean_query)
     # call the main operation function to add image to card
     add_image_to_card(card_id, clean_query, existing_image, image_field_name, env)
 
@@ -40,6 +53,7 @@ def get_image_type(image_data):
     except Exception as e:
         return None
 
+
 def add_image_to_card(card_id, query, existing_image, image_field_name, env):
     if existing_image != "":
         print(f"Skipping card {card_id} as it already has an image")
@@ -54,18 +68,22 @@ def add_image_to_card(card_id, query, existing_image, image_field_name, env):
         "key": google_api_key,
         "cx": cx,
         "num": 5,  # Number of images to return. Set to 1 to get the first image.
-        "fileType": "jpg"
+        "fileType": "jpg",
     }
     try:
-        response = requests.get("https://www.googleapis.com/customsearch/v1", params=params)
+        response = requests.get(
+            "https://www.googleapis.com/customsearch/v1", params=params
+        )
         if response.status_code != 200:
-            raise Exception(f"Received non-200 response from Google Custom Search API: {response.status_code}")
+            raise Exception(
+                f"Received non-200 response from Google Custom Search API: {response.status_code}"
+            )
         data = response.json()
 
         if "items" not in data or not data["items"]:
             print(f"No images found for query: {query}")
             return
-        
+
         for attempt in range(MAX_IMAGE_RETRIES):
             image_url = data["items"][attempt]["link"]
             image_data = requests.get(image_url).content
@@ -79,19 +97,17 @@ def add_image_to_card(card_id, query, existing_image, image_field_name, env):
             image_filename = f"{card_id}.{image_type}"
             filepath = os.path.join(media_folder, image_filename)
 
-            with open(filepath, 'wb') as f:
+            with open(filepath, "wb") as f:
                 f.write(image_data)
-            
+
             if is_valid_image(filepath):
                 # Update the Anki card to reference the image by its filename
                 field_data = f'<img src="{image_filename}">'
                 params = {
-                    "note": {
-                        "id": card_id,
-                        "fields": {image_field_name: field_data}
-                    }
+                    "note": {"id": card_id, "fields": {image_field_name: field_data}}
                 }
                 from anki_patcher.patcher.anki import invoke
+
                 invoke("updateNoteFields", params)
                 print(f"Added image to card {card_id}")
                 break
--- anki_patcher/util.py
+++ anki_patcher/util.py
@@ -3,6 +3,7 @@ import re
 import regex
 from bs4 import BeautifulSoup
 
+
 def parse_env(vars):
     env_values = []
     for var in vars:
@@ -11,6 +12,7 @@ def parse_env(vars):
             raise Exception(f"Environment variable {var} must be set")
         env_values.append(value)
     return env_values
+
 
 def parse_config(props, config):
     config_values = []
@@ -21,47 +23,64 @@ def parse_config(props, config):
         config_values.append(value)
     return config_values
 
+
 def parse_fields(field_names, fields):
     field_values = []
     for field_name in field_names:
         field_value = fields[field_name].get("value")
         if field_name == None:
             raise Exception(f"Field {field_name} must be set")
-        
+
         field_values.append(field_value)
     return field_values
+
 
 def remove_html_tags_bs(input_text):
     soup = BeautifulSoup(input_text, "html.parser")
     return soup.get_text()
 
+
+def remove_html_tags(string):
+    # Use a regular expression to match HTML tags and replace them with an empty string
+    clean_string = re.sub(r"<[^>]*>", "<>", string)
+    clean_string = re.sub(r"(<>)+", ", ", clean_string)
+    return clean_string
+
+
 def remove_furiganas(string):
     # removes all furigana written with square or round brackets
     # e.g. 家族[かぞく] -> 家族
-    return re.sub(r'(\[.*?\])|(\(.*?\))', '', string)
+    return re.sub(r"(\[.*?\])|(\(.*?\))", "", string)
+
+
 import regex
+
 
 def clean_text_for_tts(input_text):
     # Keeping letters (both Latin and Japanese), Japanese characters, and spaces
     # Removing numbers and special characters
-    cleaned_text = regex.sub(r'[^\p{L}\p{N}\p{Zs}\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}]', '', input_text)
+    cleaned_text = regex.sub(
+        r"[^\p{L}\p{N}\p{Zs}\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}]",
+        "",
+        input_text,
+    )
     return cleaned_text
 
 
 def trim_list_points(string, max_root_items, max_nested_items):
-    soup = BeautifulSoup(string, 'html.parser')
+    soup = BeautifulSoup(string, "html.parser")
 
     def trim_list(list_element, max_items):
-        for i, li in enumerate(list_element.find_all('li', recursive=False)):
+        for i, li in enumerate(list_element.find_all("li", recursive=False)):
             # Trim nested lists first
-            for nested_list in li.find_all(['ul', 'ol'], recursive=False):
+            for nested_list in li.find_all(["ul", "ol"], recursive=False):
                 trim_list(nested_list, max_nested_items)
 
             # Remove list items beyond the limit
             if i >= max_items:
                 li.decompose()
 
-    lists = soup.find_all(['ul', 'ol'])
+    lists = soup.find_all(["ul", "ol"])
     for list_element in lists:
         trim_list(list_element, max_root_items)
 
